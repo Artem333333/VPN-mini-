517c4591a33af387df6c9739095ddd919e3a01f8
Artem333333 <artem12.08200789s@gmail.com>
2026-02-04 22:02:10 +0200

    2.0

--- lib/core.rb
+++ lib/core.rb
@@ -4,8 +4,6 @@ require 'security/keys'
 require 'network/nat_pmp'
 require 'discovery/mdns_reflector'
 
-
-
 module HomeNexus
   class App
     def initialize
@@ -14,20 +12,40 @@ module HomeNexus
     end
 
     def run
-      puts "=== HomeNexus запущен  ==="
-      
+      puts "=== HomeNexus ==="
       
       keys = Security::KeyManager.get_keys
-      puts "[+] Публичный ключ: #{keys[:public]}"
+      puts "[+] Public key server: #{keys[:public]}"
 
-      puts "[*] Включаю маршрутизацию трафика (IPv4/IPv6)..."
+      puts "[*] Configuring system forwarding (IPv4/IPv6)..."
       system("powershell -Command \"Set-NetIPInterface -Forwarding Enabled\"")
       system("powershell -Command \"Set-NetIPInterface -AddressFamily IPv6 -Forwarding Enabled\"")
       
+      setup_tailscale
+
       Discovery::MdnsReflector.start
 
-      puts "[ГОТОВО] Сервер работает. Порт 51820 UDP открыт."
+      Network::NatPmp.open_port(@config['network']['listen_port'])
+
+      puts "[ГОТОВО] Server is active. Exit point: Local network."
+      puts "[*] Waiting for connections..."
       loop { sleep 10 }
+    end
+
+    private
+
+    def setup_tailscale
+      puts "[*] Integration with Tailscale (NAT Traversal)..."
+      routes = @config['network']['home_subnet']
+      ts_cmd = "tailscale up --advertise-exit-node --advertise-routes=#{routes} --accept-routes"
+      
+      if system(ts_cmd)
+        puts "[+] Tailscale has been successfully configured as an Exit Node."
+        ts_ip = `tailscale ip -4`.strip
+        puts "[+] Your address on the Tailscale network: #{ts_ip}"
+      else
+        puts "[!] Error: Make sure Tailscale is installed and running."
+      end
     end
   end
 end
